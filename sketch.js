b72e2707f3089bd39c8318c4563c9f433182388f
HLuksic <hluksic20@foi.hr>
2025-07-26 14:48:39 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+const WINDOW_WIDTH = 800;
+const WINDOW_HEIGHT = 600;
+const BLACK = [0, 0, 0];
+const GREY = [128, 128, 128];
+const WHITE = [255, 255, 255];
+
+function setup() {
+    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+}
+
+function draw() {
+    background(255);
+}
